41b7e2d3c2a430b9ef5cbe60137eb1f1b8b2273a
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-05-09 14:50:40 -0400

    Move it to localai

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -6,12 +6,7 @@ import { NextSeo } from "next-seo"
 import { BasicLayout } from "~features/layouts/base"
 import { DemoButton } from "~features/press/demo-button"
 
-const FeatureCard = ({
-  Icon = TriangleFlag,
-  name = "User Journey Analysis",
-  description = `Discover the path that led to security breaches with Jalias's
-  advanced user journey analysis.`
-}) => (
+const FeatureCard = ({ Icon = TriangleFlag, name = "", description = `` }) => (
   <div className="p-8 md:w-1/3 w-full">
     <div className="bg-mauve-3 text-mauve-11 flex flex-col items-center p-12 rounded">
       <div className="rounded-full bg-mauve-6 w-min p-4 mb-6">
@@ -26,15 +21,15 @@ const FeatureCard = ({
 const IndexPage: NextPage = () => (
   <BasicLayout>
     <NextSeo
-      title="Streamline Cybersecurity Response for Blue Teams in Enterprise Companies"
-      titleTemplate="%s – Jalias"
-      description="Jalias is a cutting-edge cybersecurity response platform specifically designed for Blue Teams in large enterprise companies. Our advanced AI-driven solution streamlines the incident response process by analyzing user journeys to determine the path that led to security breaches. Jalias seamlessly integrates with SIEM, SOAR, EDR, NSM, and SOC tools to provide a comprehensive and efficient cybersecurity response."
+      title=""
+      titleTemplate="%s – LocalAI"
+      description="LocalAI enables developers to quickly test AI models in their local machine."
       openGraph={{
-        title: "Uncover the How and Why of Security Incidents with Jalias",
+        title: "Quickly test AI models in your local machine.",
         description:
-          "Discover the how and why behind security incidents with Jalias, an AI-driven cybersecurity response platform for Blue Teams in large enterprise companies.",
-        url: "https://www.jalias.com",
-        siteName: "Jalias",
+          "LocalAI enables developers to quickly test AI models in their local machine.",
+        url: "https://www.localai.app",
+        siteName: "LocalAI",
         type: "website"
       }}
     />
@@ -43,13 +38,12 @@ const IndexPage: NextPage = () => (
     <section className="py-20">
       <div className="container mx-auto px-8 flex flex-col items-center justify-center h-full">
         <h1 className="text-5xl md:text-6xl mb-8 text-mauve-12 text-center font-medium max-w-5xl">
-          Cybersecurity response{" "}
-          <span className="text-blue-9">streamlined</span> for large
-          Enterprises.
+          Local Model Development{" "}
+          <span className="text-blue-9">streamlined</span> for Developers.
         </h1>
         <p className="text-xl mb-16 text-center max-w-xl">
-          An LLM-driven solution that produces actionable visibility to incident
-          response, enabling faster decision making for SOC analysts.
+          A native app that allow you to test out your AI models in your local
+          machine.
         </p>
         <DemoButton />
       </div>
@@ -61,35 +55,7 @@ const IndexPage: NextPage = () => (
         className={clsx(
           "container mx-auto",
           "flex flex-col md:flex-row md:flex-wrap md:justify-center"
-        )}>
-        {/* <!-- Feature 1 --> */}
-        <FeatureCard
-          Icon={TriangleFlag}
-          name="User Journey Analysis"
-          description={`Discover the path that led to security breaches with Jalias's advanced user journey analysis.`}
-        />
-
-        {/* <!-- Feature 2 --> */}
-        {/* <FeatureCard
-          Icon={PathArrow}
-          name="AI-Driven Incident Triage"
-          description={`Respond to incidents efficiently with Jalias's AI-driven incident triage capabilities.`}
-        /> */}
-
-        {/* <!-- Feature 3 --> */}
-        <FeatureCard
-          Icon={Tools}
-          name="Seamless Tool Integration"
-          description={`Integrate Jalias with your existing SIEM, SOAR, EDR, NSM, and SOC tools for a comprehensive cybersecurity response.`}
-        />
-
-        {/* <!-- Feature 4 --> */}
-        <FeatureCard
-          Icon={WarningWindow}
-          name="Alert Management"
-          description={`Customize alert management to suit your enterprise's unique needs and enhance incident response.`}
-        />
-      </div>
+        )}></div>
     </section>
   </BasicLayout>
 )
