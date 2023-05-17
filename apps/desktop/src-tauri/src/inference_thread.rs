0e1867c7750fd0d725bbe820f80faf61fd68872b
Louis <6723574+louisgv@users.noreply.github.com>
2023-05-16 23:04:07 -0400

    Doing all the manual work and borrow the model only needed

--- apps/desktop/src-tauri/src/inference_thread.rs
+++ apps/desktop/src-tauri/src/inference_thread.rs
@@ -2,9 +2,14 @@ use std::{convert::Infallible, sync::Arc};
 
 use actix_web::web::Bytes;
 use flume::Sender;
-use llm::{InferenceFeedback, InferenceParameters, InferenceRequest, InferenceResponse, Model};
+use llm::{
+    InferenceError, InferenceFeedback, InferenceParameters, Model, OutputRequest, TokenUtf8Buffer,
+};
 use parking_lot::RwLock;
-use rand::thread_rng;
+
+use rand::prelude::*;
+
+use rand_chacha::ChaCha8Rng;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -16,6 +21,17 @@ pub struct CompletionRequest {
 }
 
 pub type ModelGuard = Arc<RwLock<Option<Box<(dyn Model)>>>>;
+
+pub fn with_model<T, F: FnOnce(&Box<(dyn Model)>) -> T>(
+    model_guard: &ModelGuard,
+    f: F,
+) -> Result<T, &'static str> {
+    let guard = model_guard.read();
+    guard
+        .as_ref()
+        .map(|m| f(m))
+        .ok_or("Model locked, cannot be loaded")
+}
 
 pub struct InferenceThreadRequest {
     pub tx_token: Sender<Bytes>,
@@ -53,75 +69,150 @@ fn clean_prompt(s: &str) -> String {
 
 pub fn spawn_inference_thread(req: InferenceThreadRequest) {
     rayon::spawn(move || {
-        let guard = req.model_guard.read();
+        let mut session = with_model(&req.model_guard, |model| {
+            model.start_session(Default::default())
+        })
+        .unwrap();
 
-        let model = match &*guard {
-            Some(m) => m,
-            None => {
-                println!("Model locked, not loaded");
-                return;
-            }
-        };
-        let mut session = model.start_session(Default::default());
+        let mut rng = ChaCha8Rng::seed_from_u64(42);
 
-        let mut rng = thread_rng();
-
-        let raw_prompt = clean_prompt(req.completion_request.prompt.as_str()).clone();
+        let raw_prompt = clean_prompt(req.completion_request.prompt.as_str());
         let prompt = &raw_prompt;
         let tx_token = req.tx_token.clone();
+        let mut output_request = OutputRequest::default();
+
+        let inference_params = &InferenceParameters {
+            // n_batch: 4,
+            // n_threads: 2,
+            ..Default::default()
+        };
+
+        // Manual tokenization if needed
+        // let vocab = model.vocabulary();
+        // let prompt_tokens: Vec<i32> = vocab
+        //     .tokenize(prompt, true)
+        //     .into_par_iter() // Flatten to create a parallel iterator over the tuples
+        //     .flatten() // Flatten the nested vectors
+        //     .map(|(_, tok)| tok)
+        //     .collect();
+
+        // for batch in prompt_tokens.chunks(inference_params.n_batch) {
+        //     model.evaluate(&mut session, inference_params, batch, &mut output_request);
+        //     for &tk in batch {
+        //         // Update the tokens for this session
+        //         session.tokens.push(tk);
+        //     }
+        // }
+
+        with_model(&req.model_guard, |model| {
+            session.feed_prompt::<Infallible>(
+                model.as_ref(),
+                &inference_params,
+                prompt,
+                &mut output_request,
+                |_| Ok(InferenceFeedback::Continue),
+            )
+        })
+        .unwrap()
+        .unwrap();
+
+        let mut tokens_processed = 0;
+        let maximum_token_count = usize::MAX;
+
+        let mut token_utf8_buf = TokenUtf8Buffer::new();
+
+        while tokens_processed < maximum_token_count {
+            if *Arc::clone(&req.abort_token).read() {
+                break;
+            }
+            let mut end_of_text = false;
+            with_model(&req.model_guard, |model| {
+                let token = match session.infer_next_token(
+                    model.as_ref(),
+                    &inference_params,
+                    &mut output_request,
+                    &mut rng,
+                ) {
+                    Ok(t) => t,
+                    Err(InferenceError::EndOfText) => {
+                        end_of_text = true;
+                        &[0]
+                    }
+                    Err(e) => {
+                        println!("{}", e.to_string());
+                        end_of_text = true;
+                        &[0]
+                    }
+                };
+
+                // Buffer the token until it's valid UTF-8, then call the callback.
+                if !end_of_text {
+                    if let Some(tokens) = token_utf8_buf.push(token) {
+                        tx_token.try_send(get_completion_resp(tokens)).unwrap();
+                    }
+                }
+            })
+            .unwrap();
+
+            if end_of_text {
+                break;
+            }
+
+            tokens_processed += 1;
+        }
 
         // Run inference
-        let res = session.infer::<Infallible>(
-            model.as_ref(),
-            &mut rng,
-            &InferenceRequest {
-                prompt,
-                play_back_previous_tokens: false,
-                // maximum_token_count: Some(8472),
-                parameters: Some(&InferenceParameters {
-                    // n_batch: 4,
-                    // n_threads: 2,
-                    ..Default::default()
-                }),
-                ..Default::default()
-            },
-            // OutputRequest
-            &mut Default::default(),
-            |r| match r {
-                InferenceResponse::InferredToken(t) => {
-                    tx_token.try_send(get_completion_resp(t)).unwrap();
-                    // for ch in t.chars() {
-                    //     // for each character in t, send a completion response
-                    //     tx.try_send(get_completion_resp(ch.to_string())).unwrap();
-                    // }
+        // let res = session.infer::<Infallible>(
+        //     model.as_ref(),
+        //     &mut rng,
+        //     &InferenceRequest {
+        //         prompt,
+        //         play_back_previous_tokens: false,
+        //         // maximum_token_count: Some(8472),
+        //         parameters: Some(&InferenceParameters {
+        //             // n_batch: 4,
+        //             // n_threads: 2,
+        //             ..Default::default()
+        //         }),
+        //         ..Default::default()
+        //     },
+        //     // OutputRequest
+        //     &mut Default::default(),
+        //     |r| match r {
+        //         InferenceResponse::InferredToken(t) => {
+        //             tx_token.try_send(get_completion_resp(t)).unwrap();
+        //             // for ch in t.chars() {
+        //             //     // for each character in t, send a completion response
+        //             //     tx.try_send(get_completion_resp(ch.to_string())).unwrap();
+        //             // }
 
-                    Ok(if *Arc::clone(&req.abort_token).read() {
-                        InferenceFeedback::Halt
-                    } else {
-                        InferenceFeedback::Continue
-                    })
-                }
-                _ => Ok(if *Arc::clone(&req.abort_token).read() {
-                    InferenceFeedback::Halt
-                } else {
-                    InferenceFeedback::Continue
-                }),
-            },
-        );
+        //             Ok(if *Arc::clone(&req.abort_token).read() {
+        //                 InferenceFeedback::Halt
+        //             } else {
+        //                 InferenceFeedback::Continue
+        //             })
+        //         }
+        //         _ => Ok(if *Arc::clone(&req.abort_token).read() {
+        //             InferenceFeedback::Halt
+        //         } else {
+        //             InferenceFeedback::Continue
+        //         }),
+        //     },
+        // );
 
-        match res {
-            Ok(result) => {
-                println!(
-                    "\n\n===\n\nInference stats:\n\n{}\naborted: {}\n",
-                    result,
-                    *Arc::clone(&req.abort_token).read()
-                );
-            }
-            Err(err) => {
-                tx_token
-                    .try_send(get_completion_resp(err.to_string()))
-                    .unwrap();
-            }
-        }
+        // match res {
+        //     Ok(result) => {
+        //         println!(
+        //             "\n\n===\n\nInference stats:\n\n{}\naborted: {}\n",
+        //             result,
+        //             *Arc::clone(&req.abort_token).read()
+        //         );
+        //     }
+        //     Err(err) => {
+        //         tx_token
+        //             .try_send(get_completion_resp(err.to_string()))
+        //             .unwrap();
+        //     }
+        // }
     })
 }
