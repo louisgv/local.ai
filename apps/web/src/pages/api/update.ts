8f485aaa53089551132229c3b67b24c8fda39759
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-06-06 01:12:50 -0400

    bump version

--- apps/web/src/pages/api/update.ts
+++ apps/web/src/pages/api/update.ts
@@ -7,14 +7,15 @@ export const config = {
 const releaseJsonUrl = `${baseUrl}/latest.json`
 
 export default async function handler() {
-  const releaseJson = await fetch(releaseJsonUrl).then((res) => res.text())
+  return fetch(releaseJsonUrl)
+  // const releaseJson = await fetch(releaseJsonUrl).then((res) => res.text())
 
-  return new Response(releaseJson, {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-      "cache-control":
-        "public, s-maxage=2592000, max-age=0, stale-while-revalidate=600"
-    }
-  })
+  // return new Response(releaseJson, {
+  //   status: 200,
+  //   headers: {
+  //     "content-type": "application/json",
+  //     "cache-control":
+  //       "public, s-maxage=2592000, max-age=0, stale-while-revalidate=600"
+  //   }
+  // })
 }
