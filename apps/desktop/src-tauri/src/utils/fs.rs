efa18ddf94955ea4a58f97e1425c9ef8b9b12195
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-06-15 15:00:19 -0700

    use rustfmt

--- apps/desktop/src-tauri/src/utils/fs.rs
+++ apps/desktop/src-tauri/src/utils/fs.rs
@@ -1,28 +1,25 @@
 #[tauri::command]
 pub fn open_directory(path: String) {
-    #[cfg(target_os = "windows")]
-    {
-        use std::process::Command;
+  #[cfg(target_os = "windows")]
+  {
+    use std::process::Command;
 
-        Command::new("explorer")
-            .args(["/select,", &path]) // The comma after select is not a typo
-            .spawn()
-            .unwrap();
-    }
+    Command::new("explorer")
+      .args(["/select,", &path]) // The comma after select is not a typo
+      .spawn()
+      .unwrap();
+  }
 
-    #[cfg(target_os = "linux")]
-    {
-        use std::process::Command;
+  #[cfg(target_os = "linux")]
+  {
+    use std::process::Command;
 
-        Command::new("xdg-open")
-            .arg(&path)
-            .spawn()
-            .unwrap();
-    }
+    Command::new("xdg-open").arg(&path).spawn().unwrap();
+  }
 
-    #[cfg(target_os = "macos")]
-    {
-        use std::process::Command;
-        Command::new("open").args(["-R", &path]).spawn().unwrap();
-    }
+  #[cfg(target_os = "macos")]
+  {
+    use std::process::Command;
+    Command::new("open").args(["-R", &path]).spawn().unwrap();
+  }
 }
