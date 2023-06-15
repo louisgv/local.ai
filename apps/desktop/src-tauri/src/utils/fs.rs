f951ce3ca92d5cd6df5bf9528f70d722af4fd99a
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-06-15 16:38:09 -0700

    rustfmt

--- apps/desktop/src-tauri/src/utils/fs.rs
+++ apps/desktop/src-tauri/src/utils/fs.rs
@@ -12,23 +12,20 @@ pub fn open_directory(path: String) {
 
   #[cfg(target_os = "linux")]
   {
-    use std::process::Command;
-    use std::path::{Path, PathBuf};
     use std::fs::metadata;
+    use std::path::{Path, PathBuf};
+    use std::process::Command;
 
-      let new_path = match metadata(&path).unwrap().is_dir() {
-        true => path,
-        false => {
-          let mut path2 = PathBuf::from(path);
-          path2.pop();
-          path2.into_os_string().into_string().unwrap()
-        }
-      };
+    let new_path = match metadata(&path).unwrap().is_dir() {
+      true => path,
+      false => {
+        let mut path2 = PathBuf::from(path);
+        path2.pop();
+        path2.into_os_string().into_string().unwrap()
+      }
+    };
 
-    Command::new("xdg-open")
-      .arg(&new_path)
-      .spawn()
-      .unwrap();
+    Command::new("xdg-open").arg(&new_path).spawn().unwrap();
   }
 
   #[cfg(target_os = "macos")]
