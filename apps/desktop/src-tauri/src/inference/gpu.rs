73c56ce6755367511bc799a908b59272422b833c
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-07-03 03:28:04 -0700

    Disable GPU for Linux and Windows for now

--- apps/desktop/src-tauri/src/inference/gpu.rs
+++ apps/desktop/src-tauri/src/inference/gpu.rs
@@ -1,9 +1,9 @@
 #[tauri::command]
-pub async fn check_gpu<'a>() -> Result<bool, String> {
-  #[cfg(target = "aarch64-apple-darwin")]
-  {
-    return Ok(true);
+pub async fn check_gpu() -> Result<bool, String> {
+  // TODO: actually check if Metal is available in the future (?)
+  if cfg!(all(target_os = "macos", target_arch = "aarch64")) {
+    Ok(true)
+  } else {
+    Ok(false)
   }
-
-  Ok(false)
 }
